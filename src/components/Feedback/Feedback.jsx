8350ca1e9d6aa84f735b550c0967ee0e89115d5c
Galina Lasko <galina_lasko@hotmail.com>
2024-03-22 22:22:22 +0100

    added homework31

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,10 @@
+function Feedback() {
+  return (
+    <div>
+      <div>Палец вверх</div>
+      <div>Палец вниз</div>
+      <div>Сброс</div>
+    </div>
+  )
+}
+export default Feedback;
---
+++ src/homeworks/Homework31/Homework31.tsx
@@ -0,0 +1,8 @@
+import Counter from "components/Counter/Counter";
+
+//import Feedback from 'components/Feedback/Feedback';
+function Homework31()
+{
+    return  <Counter/> 
+}
+export default Homework31;
